f05b7551620f4a9311749304487a6005ba4f3556
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2025-12-10 01:50:26 +0100

    feat: improve entrypoint
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 
 repo=${GITHUB_README_STATS_REPO:-"https://github.com/anuraghazra/github-readme-stats.git"}
 ref=${GITHUB_README_STATS_REF:-"master"}
+fix_audit=${FIX_AUDIT:-"true"}
 
 if [ -z "${PAT_1:-}" ]; then
   echo "Error: PAT_1 environment variable is not set. Define at least one GH Personal Access Token for this app to work."
@@ -11,15 +12,25 @@ if [ -z "${PAT_1:-}" ]; then
 fi
 
 echo "Cloning repository $repo (ref: $ref)..."
-git clone --depth 1 --branch "$ref" "$repo" /repo
 
+mkdir -p /repo
 cd /repo
+git init --initial-branch=main
+git config advice.detachedHead false
+git remote add origin "$repo"
+git fetch --depth 1 origin "$ref"
+git checkout FETCH_HEAD
 
 echo "Moving express dependency from devDependencies to dependencies in package.json..."
 
 # Use jq to move express from devDependencies to dependencies
-jq '.dependencies.express = .devDependencies.express | del(.devDependencies.express)' package.json > package.tmp.json
+jq '.dependencies.express = .devDependencies.express | del(.devDependencies.express)' package.json >package.tmp.json
 mv package.tmp.json package.json
+
+if [ "$fix_audit" = "true" ]; then
+  echo "Fixing audit issues in package-lock.json..."
+  npm audit fix
+fi
 
 echo "Installing production dependencies..."
 npm install
